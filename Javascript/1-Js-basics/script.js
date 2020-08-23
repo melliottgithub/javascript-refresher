ac3af5fba608227102920cca7666c98a510f777d
Michael <kllmmc23@gmail.com>
2020-08-22 20:39:35 -0600

    different-data-types

--- Javascript/1-Js-basics/script.js
+++ Javascript/1-Js-basics/script.js
@@ -130,7 +130,7 @@ Retirement(1990, "John");
 // Function declaration
 // function whatDoYouDo(job, firstName) {}
 
-// Function expression
+// Function expression and expressions
 var whatDoYouDo = function(job, firstName) {
     switch(job) {
         case 'teacher':
@@ -147,3 +147,37 @@ var whatDoYouDo = function(job, firstName) {
 console.log(whatDoYouDo('teacher', 'John'));
 console.log(whatDoYouDo('designer', 'Jane'));
 console.log(whatDoYouDo('retired', 'Mark'));
+
+/*****************************
+* Arrays
+*/
+// Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+console.log(names.length);
+
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(23));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
+
+
