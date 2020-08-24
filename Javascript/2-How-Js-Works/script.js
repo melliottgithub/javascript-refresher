d27b73e939857868f25f71d4009dbcb37a5681b1
Michael <kllmmc23@gmail.com>
2020-08-24 08:29:46 -0600

    this

--- Javascript/2-How-Js-Works/script.js
+++ Javascript/2-How-Js-Works/script.js
@@ -22,10 +22,80 @@ var age = 23;
 console.log(age);
 
 function foo() {
-    console.log(age);
+  console.log(age);
   var age = 65;
   console.log(age);
 }
 
 foo();
 console.log(age);
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+// First scoping example
+
+var a = "Hello!";
+first();
+
+function first() {
+  var b = "Hi!";
+  second();
+
+  function second() {
+    var c = "Hey!";
+    console.log(a + b + c);
+  }
+}
+
+// Example to show the difference between execution stack and scope chain
+
+var a = "Hello!";
+first();
+
+function first() {
+  var b = "Hi!";
+  second();
+
+  function second() {
+    var c = "Hey!";
+    third();
+  }
+}
+
+function third() {
+  var d = "John";
+  console.log(a + d);
+  //console.log(a + b + c + d);
+}
+
+//this
+
+console.log(this);
+
+calculateAge(1985);
+
+//function declaration
+function calculateAge(year) {
+  console.log(2018 - year);
+  console.log(this);
+}
+
+var john = {
+  name: "John",
+  birth: 1990,
+  calculateAge: function () {
+    console.log(this);
+    console.log(2020 - this.birth);
+  },
+};
+
+john.calculateAge();
+var mike = {
+  name: "Mike",
+  birth: 1983,
+};
+
+mike.calculateAge = john.calculateAge;
+
+mike.calculateAge();
